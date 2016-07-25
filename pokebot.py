c4fd16c4948ab5e04aecea6d57bda08d177e4a37
bcc5160 <bc.chiu12@gmail.com>
2016-07-24 22:17:43 -0400

    initial files

---
+++ pokebot.py
@@ -0,0 +1,29 @@
+# =====================================================================================================================================================
+# Pokebot will randomly have pokemon appear in chats.
+#   Only 151 are here
+# ** MUST HAVE pyTelegramBotAPI : [pip install pyTelegramBotAPI] or upgrade [pip install pytelegrambotapi --upgrade]
+# How to run: python3 pokebot.py 
+# =====================================================================================================================================================
+
+# Import libs
+import telebot
+import configparser
+from random import randint
+
+# Parse config file to get the API key
+config = configparser.ConfigParser()
+config.read("pokebot.cfg")
+
+TOKEN = config['telegram_bot_API']['API_TOKEN']
+
+# Declare bot
+bot = telebot.TeleBot(TOKEN)
+
+# Message handler for /start and /help
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Catch the pokemon when they appear!")
+
+# Bot waits for events
+print("Pokebot is running...")
+bot.polling()
