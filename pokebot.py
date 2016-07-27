64b29c2b2f249009d2418b194317850b8a7c2bd9
bcc5160 <bc.chiu12@gmail.com>
2016-07-27 07:29:25 -0400

    started database

--- pokebot.py
+++ pokebot.py
@@ -2,6 +2,7 @@
 # Pokebot will randomly have pokemon appear in chats.
 #   Only 151 are here
 # ** MUST HAVE pyTelegramBotAPI : [pip install pyTelegramBotAPI] or upgrade [pip install pytelegrambotapi --upgrade]
+# ** MUST HAVE tinydb : [pip install tinydb]
 # How to run: python3 pokebot.py 
 # =====================================================================================================================================================
 
---
+++ tinydb_interface.py
@@ -0,0 +1,11 @@
+# Import libraries
+from tinydb import TinyDB, Query
+from random import randint
+import os
+import time
+
+# Order based on rarity
+
+# Probability to pick a pokemon from
+
+
